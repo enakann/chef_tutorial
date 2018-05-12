4fb9fe2c414f074abd1ceb9b64f21bec7a523a97
vagrant <vagrant@localhost.localdomain>
2018-05-12 14:37:00 +0000

    initial workstation commit

---
+++ recipes/setup.rb
@@ -0,0 +1,16 @@
+package 'tree' do
+  action:install
+end
+
+package 'ntp'
+
+package 'git' do
+   action:install
+end
+
+file '/etc/motd' do
+  content "This server is the property of kannan"
+  action:create
+  owner 'root'
+  group 'root'
+end
