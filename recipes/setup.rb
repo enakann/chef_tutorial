95c062d0b6d65b395e21a5f516b190250332d5c7
vagrant <vagrant@localhost.localdomain>
2018-05-13 13:40:30 +0000

    updated on 13th may

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -19,9 +19,16 @@ file '/home/vagrant/notes' do
    group 'kannan'
 end
 
+
+
 file '/etc/motd' do
-  content "This server is the property of kannan"
-  action:create
+  content "This server is the property of kannan
+  HOSTNAME:#{node['hostname']}
+  IPADDRESS:#{node['ipaddress']}
+  CPU:#{node['cpu']['0']['mhz']}
+  MEMORY:#{node['memory']['total']}
+"
   owner 'root'
   group 'root'
+  action:create
 end
